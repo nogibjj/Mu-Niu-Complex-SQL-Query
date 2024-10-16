568e0df5bf1e9a9c1f12ea3219a4754a4a589ab5
Mu-niu13 <olivern0711@gmail.com>
2024-10-15 21:40:13 -0400

    refine2

--- mylib/query.py
+++ mylib/query.py
@@ -1,33 +1,31 @@
 import os
-import csv
 from databricks import sql
 from dotenv import load_dotenv
 
 
-def query(dataset="data/student_performance.csv"):
-    payload = csv.reader(open(dataset, newline=""), delimiter=",")
-    next(payload)
+def query():
     load_dotenv()
     with sql.connect(server_hostname = os.getenv("SERVER_HOSTNAME"),
                      http_path = os.getenv("HTTP_PATH"),
                      access_token = os.getenv("DATABRICKS_KEY")) as connection:
-        c = connection.cursor()
-        c.execute(
-            """
-                SELECT
-                    s1.ParentalSupport,
-                    AVG(s1.ExtracurricularActivities) AS activity,
-                    AVG(s1.PreviousGrade) AS previous_grade,
-                    AVG(s1.FinalGrade) AS final_grade
-                FROM student_performance AS s1
-                JOIN student_performance AS s2
-                    USING (Name)
-                GROUP BY s1.ParentalSupport
-                ORDER BY s1.ParentalSupport DESC
+        with connection.cursor() as cursor:
+            cursor.execute(
                 """
-        )
-        c.fetchall()
-        c.close()
+                    SELECT
+                        s1.ParentalSupport,
+                        AVG(s1.ExtracurricularActivities) AS activity,
+                        AVG(s1.PreviousGrade) AS previous_grade,
+                        AVG(s1.FinalGrade) AS final_grade
+                    FROM default.student_performance AS s1
+                    JOIN default.student_performance AS s2
+                        USING (Name)
+                    GROUP BY s1.ParentalSupport
+                    ORDER BY s1.ParentalSupport DESC;
+                    """
+            )
+            cursor.close()
+            connection.close()
+
     return "Query Success"
 
 

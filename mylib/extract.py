465e7432bb8caafee032614b0cf0f08f6c87836c
Mu-niu13 <olivern0711@gmail.com>
2024-10-15 21:30:35 -0400

    refine

--- mylib/extract.py
+++ mylib/extract.py
@@ -1,17 +1,12 @@
-import os
 import requests
 
+
 def extract(
-    url="""
-    https://raw.githubusercontent.com/nogibjj/Mu-Niu-Pandas-Descriptive-Statistics-Script/main/student_performance.csv
-    """,
+    url="https://raw.githubusercontent.com/nogibjj/Mu-Niu-Pandas-Descriptive-Statistics-Script/main/student_performance.csv",
     file_path="data/student_performance.csv",
-    directory="data",
 ):
-    """Extract a url to a file path"""
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    with requests.get(url, timeout=10) as r:
+    """ "Extract a url to a file path"""
+    with requests.get(url) as r:
         with open(file_path, "wb") as f:
             f.write(r.content)
     return file_path
